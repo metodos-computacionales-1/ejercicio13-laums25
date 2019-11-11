ef4400ab96e50fdada79e7cb797b27bdd0009567
laums25 <lm.mercado@uniandes.edu.co>
2019-11-10 20:25:37 -0500

    segunda parte del cuarto ejercicio

--- 4.cpp
+++ 4.cpp
@@ -2,22 +2,39 @@
 #include <cmath>
 
 int getMaxInt(int n1, int n2);
+double getMaxDouble(double nu1, double nu2);
  
 int main () {
    int a = 500;
    int b = 100;
+   double  x=25.0;
+   double  y=47.5;
    int ret;
+   double resul;
    ret = getMaxInt(a, b);
+   resul = getMaxDouble(x,y);
    std::cout<<"El valor int máximo es :" <<ret<<std::endl;
+   std::cout<<"El valor double máximo es :"<<resul<<std::endl;
    return 0;
 }
  
 int getMaxInt(int n1, int n2) {
    int resultado;
-   if (n1 > n2)
+   if (n1 > n2){
       resultado = n1;
-   else
+}
+   else{
       resultado = n2;
+}
    return resultado; //Modificado de Ejercicios aprenderaprogramar.com
 }
-
+double getMaxDouble(double nu1, double nu2){
+   double r;
+   if (nu1 > nu2){
+   r=nu1;
+}
+   else {
+   r=nu2;
+}
+   return r;
+}
