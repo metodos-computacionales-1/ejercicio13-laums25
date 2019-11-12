441fa11fe681faf5be75163260bd1a921d233d01
laums25 <lm.mercado@uniandes.edu.co>
2019-11-11 20:07:36 -0500

    Heap instead of the Stack

---
+++ dynamic2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+int main(int argc, char **argv)
+{
+    int N=atoi(argv[1]); //casting to int
+    double *M = new double[N];
+    for(int i =0;i<N;i++)
+    {
+        M[i]=i;
+    }
+    delete[] M;	
+    return 0;
+}
